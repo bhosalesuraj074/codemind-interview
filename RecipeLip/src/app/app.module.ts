0fe252eab8159d8ece6e0a2d66c3ed00e810f7ca
Suraj Bhosale <125651189+bhosalesuraj074@users.noreply.github.com>
2024-02-29 12:36:12 +0000

    codemind

--- RecipeLip/src/app/app.module.ts
+++ RecipeLip/src/app/app.module.ts
@@ -2,18 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
